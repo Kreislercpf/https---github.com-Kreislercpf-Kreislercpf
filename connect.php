23959d8c88278b13b32c5d6bb7949250d9c1f572
Kreislercpf <91108843+Kreislercpf@users.noreply.github.com>
2023-10-20 21:23:05 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+    $SERVER_NAME = "localhost";
+    $USERNAME = "root";
+    $PASSWORD = "";
+    $DB_NAME = "wwd_database";
+
+    $conn = new mysqli($SERVER_NAME,$USERNAME,$PASSWORD,$DB_NAME);
+
+    if($conn->connect_error) {
+        die("Connection failed: ". $conn->connect_error);
+    }
+    //echo "Connection Successful...";
+    //$conn->close();
+?>
